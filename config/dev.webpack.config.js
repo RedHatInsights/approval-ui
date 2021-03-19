2e2fc489323f0686c8c70fe3dd3353f54a61df43
Laura Galis <lgalis@redhat.com>
2021-03-19 17:04:40 -0400

    Update webpack to 5.x

---
+++ config/dev.webpack.config.js
@@ -0,0 +1,21 @@
+const { resolve } = require('path');
+const config = require('@redhat-cloud-services/frontend-components-config');
+const { config: webpackConfig, plugins } = config({
+  rootFolder: resolve(__dirname, '../'),
+  debug: true,
+  https: true,
+  useFileHash: false,
+  deployment: process.env.BETA ? 'beta/apps' : 'apps'
+});
+
+plugins.push(
+  require('@redhat-cloud-services/frontend-components-config/federated-modules')({
+    root: resolve(__dirname, '../'),
+    useFileHash: false
+  })
+);
+
+module.exports = {
+  ...webpackConfig,
+  plugins
+};
