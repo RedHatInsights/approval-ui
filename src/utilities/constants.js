c38df99cb11172a398a4584665bc685701040497
Laura Galis <lgalis@redhat.com>
2022-02-11 12:27:40 -0500

    Update authorization paths

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -13,6 +13,13 @@ export const APPROVAL_API_BASE = DEPLOYMENT_MODE === 'standalone'
   ? // eslint-disable-next-line no-undef
   `${API_HOST}${API_BASE_PATH}`
   : `${process.env.BASE_PATH || '/api'}/approval/v1.2`;
+
+// eslint-disable-next-line no-undef
+export const AUTH_API_BASE = DEPLOYMENT_MODE === 'standalone'
+  ? // eslint-disable-next-line no-undef
+  `${API_HOST}${AUTH_BASE_PATH}`
+  : `${process.env.BASE_PATH || '/api'}/auth`;
+
 export const RBAC_API_BASE = `${process.env.BASE_PATH || '/api'}/rbac/v1`;
 
 export const decisionValues = {
