1a4c95b905d81d0161155cd32cabba9bf0bae552
Laura Galis <lgalis@redhat.com>
2022-03-03 15:55:46 -0500

    Add workflow test

--- src/helpers/group/group-helper.js
+++ src/helpers/group/group-helper.js
@@ -1,7 +1,8 @@
 import { getAxiosInstance } from '../shared/user-login';
 import { APPROVAL_API_BASE, RBAC_API_BASE } from '../../utilities/constants';
+import { isStandalone } from '../shared/helpers';
 
-export const fetchFilterApprovalGroups = (filterValue) => {
+const fetchFilterApprovalGroupsI = (filterValue) => {
   const filterQuery = `&name=${filterValue}`;
   return getAxiosInstance().get(`${RBAC_API_BASE}/groups/?role_names=",Approval Administrator,Approval Approver,"
   ${filterValue && filterValue.length > 0
@@ -9,10 +10,14 @@ export const fetchFilterApprovalGroups = (filterValue) => {
   .then(({ data }) => (data && data.length > 0 ? data.map(({ uuid, name }) => ({ label: name, value: uuid })) : undefined));
 };
 
-export const fetchFilterApprovalGroupsS = (filterValue) => {
+const fetchFilterApprovalGroupsS = (filterValue) => {
   const filterQuery = `&name=${filterValue}`;
   return getAxiosInstance().get(`${APPROVAL_API_BASE}/groups/?role=approval-approver&role=approval-admin
   ${filterValue && filterValue.length > 0
     ? filterQuery : ''}`)
-  .then(({ data }) => (data && data.length > 0 ? data.map(({ uuid, name }) => ({ label: name, value: uuid })) : undefined));
+  .then(({ data }) => (data && data.length > 0 ? data.map(({ id, name }) => ({ label: name, value: id })) : undefined));
+};
+
+export const fetchFilterApprovalGroups = (filterValue) => {
+  return isStandalone() ? fetchFilterApprovalGroupsS(filterValue) : fetchFilterApprovalGroupsI(filterValue);
 };
